140c6afd69656e7951c9f45f9d86f48ebbc0b3f2
Dawn <wanghaodawn@cmu.edu>
2017-01-10 01:43:07 -0500

    Changed the basic UI

--- src/main.js
+++ src/main.js
@@ -1,14 +1,33 @@
-var http = require("http");
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var util = require('util');
 
 http.createServer(function (request, response) {
 
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
+    // Get pathname 
+    var pathname = url.parse(request.url).pathname;
+    var filename = "html" + pathname;
+    console.log("Request for filename: [" + pathname + "] filename: [" + filename + "] received.");
+
+    // Read from file system for the content
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+         console.log(err);
+         // HTTP status code: 404 : NOT FOUND
+         // Content Type: text/plain
+         response.writeHead(404, {'Content-Type': 'text/html'});
+      } else {             
+         // HTTP status code: 200 : OK
+         // Content Type: text/plain
+         response.writeHead(200, {'Content-Type': 'text/html'});    
+         
+         // Response
+         response.write(data.toString());       
+      }
+      //  Send response data
+      response.end();
+  });
 }).listen(8081);
 
 // Console will print the message
